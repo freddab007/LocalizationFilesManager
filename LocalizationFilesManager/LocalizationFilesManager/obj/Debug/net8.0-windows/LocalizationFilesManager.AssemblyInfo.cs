a36853e29aba9b6e011d33dd353d4fbdc3ccdc8c
TomProfProg <tom.creajeux@gmail.com>
2024-10-22 13:08:35 +0200

    File version

--- LocalizationFilesManager/LocalizationFilesManager/obj/Debug/net8.0-windows/LocalizationFilesManager.AssemblyInfo.cs
+++ LocalizationFilesManager/LocalizationFilesManager/obj/Debug/net8.0-windows/LocalizationFilesManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351785a6d352771d947f8112a5de52c06686eaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f18a6fdc752bdcb4af1405a985e8cb8fbfc376")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
