c21d210dfedd849d6a8f005625e754922f2c0315
TomProfProg <tom.creajeux@gmail.com>
2024-10-29 10:30:36 +0100

    Changement dans le fichier Hello.txt

--- LocalizationFilesManager/LocalizationFilesManager/obj/Debug/net8.0-windows/LocalizationFilesManager.AssemblyInfo.cs
+++ LocalizationFilesManager/LocalizationFilesManager/obj/Debug/net8.0-windows/LocalizationFilesManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f18a6fdc752bdcb4af1405a985e8cb8fbfc376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36853e29aba9b6e011d33dd353d4fbdc3ccdc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalizationFilesManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
