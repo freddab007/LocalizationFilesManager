d31ac8876a980949801f310e5412fdbc22918c0b
TomProfProg <tom.creajeux@gmail.com>
2024-10-29 12:24:44 +0100

    Ajout Base Projet WPF

--- LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
+++ LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
@@ -16,9 +16,41 @@ namespace LocalizationFilesManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string[] Columns = { "Id", "en", "fr", "es", "comments" };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (string column in Columns)
+            {
+                //Exemple pour ajouter une colonne à la grille
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                //L'entête de la colonne
+
+
+                textColumn.Header = column;
+
+                //le nom programmatique de la colonne
+                textColumn.Binding = new Binding(column);
+
+                //l'ajout'
+                dataGrid.Columns.Add(textColumn);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        } 
+        private void Button_Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
