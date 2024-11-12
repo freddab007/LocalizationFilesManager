a55e5d34e10923e3b20f986b2704525ee0372452
fantome678 <louka.lenevez123@gmail.com>
2024-11-12 12:35:26 +0100

    commit

--- LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
+++ LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,32 +21,66 @@ namespace LocalizationFilesManager
     public partial class MainWindow : Window
     {
         public string[] Columns = { "Id", "en", "fr", "es", "comments" };
+        DataTable Data = new DataTable();
+        public class FileTranslate
+        {
+            public string id = "";
+            public string en = "";
+            public string fr = "";
+            public string es = "";
+            public string comments = "";
+        }
+        private void InitGrid(string[] _string)
+        {
+            foreach (string column in _string)
+            {
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                textColumn.Header = column;
+                textColumn.Binding = new Binding(column);
+                dataGrid.Columns.Add(textColumn);
+            }
+        }
+        /*private void InitGrid(string[] _string)
+        {
+
+            foreach (string column in _string)
+            {
+                //Exemple pour ajouter une colonne à la grille
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                ////L'entête de la colonne
+
+
+                textColumn.Header = column;
+
+                ////le nom programmatique de la colonne
+                textColumn.Binding = new Binding(column);
+
+                ////l'ajout'
+               /dataGrid.Columns.Add(textColumn);
+               // Data.Columns.Add(column);
+            }
+            //dataGrid.ItemsSource = Data.DefaultView;
+        }*/
+
+        private void AddGrid()
+        {
+
+            Data.Rows.Add("gfd", "", "", "", "r");
+
+            dataGrid.ItemsSource = Data.DefaultView;
+        }
 
         public MainWindow()
         {
             InitializeComponent();
 
-            foreach (string column in Columns)
-            {
-                //Exemple pour ajouter une colonne à la grille
-                DataGridTextColumn textColumn = new DataGridTextColumn();
-                //L'entête de la colonne
-
-
-                textColumn.Header = column;
-
-                //le nom programmatique de la colonne
-                textColumn.Binding = new Binding(column);
-
-                //l'ajout'
-                dataGrid.Columns.Add(textColumn);
-            }
+            InitGrid(Columns);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-        } 
+            AddGrid();
+        }
         private void Button_Edit(object sender, RoutedEventArgs e)
         {
 
@@ -54,11 +92,46 @@ namespace LocalizationFilesManager
         }
         private void ExportCSV(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV file (*.csv)|*.csv";
+            saveFileDialog.DefaultExt = ".csv";
 
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter s = new StreamWriter(saveFileDialog.FileName))
+                {
+                    for (int i = 0; i < Columns.Length; i++)
+                    {
+                        s.WriteLine(Columns[i]);
+                    }
+                }
+            }
         }
         private void ImportCSV(object sender, RoutedEventArgs e)
         {
+            dataGrid.Columns.Clear();
+            List<string> columns = new List<string>();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV file (*.csv)|*.csv";
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string text = "";
+                using (StreamReader s = new StreamReader(openFileDialog.FileName))
+                {
+                    while ((text = s.ReadLine()) != null)
+                    {
+                        columns.Add(text);
+                    }
+
+                    s.Close();
+                }
+                for (int i = 0; i < Columns.Length; i++)
+                {
+                    Columns[i] = columns[i];
+                }
+                InitGrid(Columns);
+            }
         }
         private void ExportJSON(object sender, RoutedEventArgs e)
         {
