29fde101ca24fbc0d4d652655385d7897d3572ae
freddab007 <erwann.perrin.i2d9@gmail.com>
2024-11-21 16:03:53 +0100

    ExportC# finish

--- LocalizationFilesManager/LocalizationFilesManager/ExportCS.cs
+++ LocalizationFilesManager/LocalizationFilesManager/ExportCS.cs
@@ -11,26 +11,44 @@ namespace LocalizationFilesManager
         {
             SaveFileDialog fileWriter = new SaveFileDialog();
 
-            fileWriter.Filter = "CS|.cs";
+            fileWriter.Filter = "CS|*.cs";
 
             if (fileWriter.ShowDialog() == true)
             {
                 using (StreamWriter writer = new StreamWriter(fileWriter.FileName))
                 {
 
-                    writer.WriteLine("using System.Globalization;");
+                    writer.WriteLine("using System.Globalization;\n");
+
+                    writer.WriteLine("public enum Language");
+                    writer.WriteLine("{");
+                    for (int i = 1; i < _data.Columns.Count - 1; i++)
+                    {
+                        writer.WriteLine("    " + _data.Columns[i].ColumnName.ToUpper() + ",");
+                    }
+                    writer.WriteLine("}\n\n");
+
                     writer.WriteLine("public class LocalizationLanguage");
                     writer.WriteLine("{");
-                    for (int i = 0; i < _data.Columns.Count - 1; i++)
+                    writer.WriteLine("    static Dictionary<string, Dictionary<Enum, string>> dictionaryWord = new Dictionary<string, Dictionary<Enum, string>>();\n");
+
+
+                    writer.WriteLine("    static public void InitDictionary()");
+                    writer.WriteLine("    {");
+
+
+
+                    for (int i = 0; i < _data.Rows.Count; i++)
                     {
-                        writer.WriteLine("  static Dictionary<string, List<string>> " + _data.Columns[i].ColumnName + "Word = new Dictionary<string, List<string>>();");
+                        writer.WriteLine("        dictionaryWord.Add(\"" + _data.Rows[i].ItemArray[0].ToString() + "\", new Dictionary<Enum, string>);");
+                        for (int j = 1; j < _data.Columns.Count - 1; j++)
+                        {
+                            writer.WriteLine("        dictionaryWord[\"" + _data.Rows[i].ItemArray[0].ToString() + "\"].Add( Language." + _data.Columns[j].ColumnName.ToUpper() + ", \"" + _data.Rows[i].ItemArray[j] +"\");");
+                        }
+                        writer.WriteLine("");
                     }
 
-                    writer.WriteLine("  static public void InitDictionary()");
-                    writer.WriteLine("  {");
-                    writer.WriteLine("      dictionaryWord.Add(\"" + _data.Columns[1] + "\", new List<string>());");
-
-                    writer.WriteLine("  }");
+                    writer.WriteLine("    }");
 
 
 
